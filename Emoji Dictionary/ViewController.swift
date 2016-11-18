450b9ba09a47b465798c7fca49de8dfbb4fb41f2
Jorge <jlvelasco1994@gmail.com>
2016-11-18 10:37:52 -0600

    Added dataSource and delegate for the tableview, created emoji array that passes info to the cell. Added a second ViewController BUT NO SEGUE YET! App is building though.

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var mainEmojiTableView: UITableView!
+    
+    var emojis = ["💩","😂","😍","🐿","🐹", "🦁"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mainEmojiTableView.dataSource = self
+        mainEmojiTableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return emojis.count //how many cells are gonna be is the ammount of items in "emojis" array
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row] //it's going to ad to each row whatever is in te emoji's array
+        return cell // finish returning "cell" constant variable 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
